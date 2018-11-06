a4a8f440a2dfb4186c3103f03c2443ef85f46beb
elhenro <github@henry-schober.com>
2018-11-06 18:57:06 +0100

    json testdata in vue

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div v-for="ride in api" class="ride">
+      {{ride.line.name}}
+      {{ride.direction}}
+      {{ride.when}}
+    </div>
+    {{api}} 
+  </div>
+</template>
+
+<script>
+
+import json from './../data/data.json'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      api: json
+    }
+  }
+}
+</script>
+
+<style scoped lang="sass">
+.home
+  .ride
+    margin: 1em
+</style>
